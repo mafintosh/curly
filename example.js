4084ffbde85e932ea2633050b07b544b3e7d0bda
Mathias Buus <mathiasbuus@gmail.com>
2012-06-26 14:17:58 +0200

    updated example to use 0.5

--- example.js
+++ example.js
@@ -6,3 +6,16 @@ curly.get('http://api.geonames.org/citiesJSON', {
 }, function(err, res, body) {
 	console.log(res.body);
 });
+
+var geonames = curly.use(function(options, send) {
+	options.url = 'http://api.geonames.org'+options.url;
+	options.json = true;
+	options.query = options.query || {};
+	options.query.username = 'demo';
+	options.query.lang = 'de';
+	return send(options);
+});
+
+geonames('/citiesJSON', {query:{north:'44.1',south:'-9.9', east:'-22.4', west:'55.2'}}, function(err, res, body) {
+	console.log(body);
+});
